6b97201221cb1f51b00226cf5fc158b862f3d1b6
Infantaria0701 <andre05062001@gmail.com>
2025-06-09 11:50:52 -0300

    criando um projeto angular

---
+++ escola-app/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import{ListaDeDisciplinasComponent} from './lista-de-disciplinas/lista-de-disciplinas.component'
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,ListaDeDisciplinasComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'escola-app';
+}
+
