6003b788c5f08b676927c63bd5536bdcddde486a
Infantaria0701 <andre05062001@gmail.com>
2025-06-11 12:11:21 -0300

    aula 11/06

--- escola-app/src/app/app.component.ts
+++ escola-app/src/app/app.component.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import{ListaDeDisciplinasComponent} from './lista-de-disciplinas/lista-de-disciplinas.component'
+import{ ListaDeDisciplinasComponent} from "./lista-de-disciplinas/lista-de-disciplinas.component"
+import { CommonModule } from '@angular/common';
+import { Disciplina } from './disciplina.model';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,ListaDeDisciplinasComponent],
+  imports: [RouterOutlet,ListaDeDisciplinasComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'escola-app';
+  selecionado: null | Disciplina = null
+
+    disciplinas = [
+      new Disciplina("Lingua Portuguesa", "Essa matéria fala portugues")
+    ];
+    
+    selecionar (disciplina:Disciplina){
+      this.selecionado=disciplina
+    }
+
+    
+   
+   
+
 }
+ 
+
 
